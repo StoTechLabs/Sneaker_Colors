9ca551eef4035c0b47ff94f18b4a33f80b12bccc
Joshua Storey <jstorey@joshuas-mbp.domain>
2014-08-19 10:43:02 -0700

    create attr_reader with attributes for functionality to call those on sneaker class. Spec passes.

--- lib/sneaker.rb
+++ lib/sneaker.rb
@@ -1,8 +1,10 @@
 class Sneaker
 
+	attr_reader :brand, :name
+
 	def initialize(attributes)
-
-
+		@brand = attributes[:brand]
+		@style = attributes[:name]
 	end 
 	
 end
--- spec/sneaker_spec.rb
+++ spec/sneaker_spec.rb
@@ -4,10 +4,20 @@ describe Sneaker do
 
 	describe "initialize" do
 		it 'should initialize sneaker class with a brand and style' do
-			test_sneaker = Sneaker.new({:brand => 'Nike'});
+			test_sneaker = Sneaker.new({:brand => 'Nike', :style => 'Yeezy'})
 			expect(test_sneaker).to be_an_instance_of Sneaker
 		end
+
+		it 'should display atributes when they are called on the class' do
+			test_sneaker = Sneaker.new({:brand => 'Nike', :style => 'Yeezy'})
+			expect(test_sneaker.brand).to eq 'Nike'
+		end
+
 	end
+
+
+
+
 
 
 
