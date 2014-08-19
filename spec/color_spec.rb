81d172a2c96cea943ec184196c129afdb35e31dd
Joshua Storey <jstorey@joshuas-mbp.domain>
2014-08-19 12:13:33 -0700

    Create function to initializeColor class with a name of a color.

---
+++ spec/color_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Color do 
+
+	describe "initalized" do
+		it "should initalize the color class with a name of a color" do
+			test_color = Color.new({:name => 'Solar Red'})
+			expect(test_color).to be_an_instance_of Color
+		end
+	end
+
+	
+end
