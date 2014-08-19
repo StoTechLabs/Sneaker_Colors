f9847abde3acb04d24290d7da5b4dec9ee00563b
Joshua Storey <jstorey@joshuas-mbp.domain>
2014-08-19 15:15:12 -0700

    Create test to make sure id attributes are defined with attr_reader. Spec passes.

--- lib/sneaker_colorway.rb
+++ lib/sneaker_colorway.rb
@@ -1,5 +1,7 @@
 class Sneaker_Colorway 
 
+	attr_reader :sneaker_id, :color_id
+
 	def initialize(attributes)
 		@sneaker_id = attributes[:sneaker_id]
 		@color_id = attributes[:color_id]
--- spec/sneaker_colorway_spec.rb
+++ spec/sneaker_colorway_spec.rb
@@ -7,11 +7,13 @@ describe 'Sneaker_Colorway' do
 			test_colorway = Sneaker_Colorway.new({:sneaker_id => 1, :color_id => 2})
 			expect(test_colorway).to be_an_instance_of Sneaker_Colorway
 		end
+
+		it 'should display attributes when they are called on the class' do
+			test_colorway = Sneaker_Colorway.new({:sneaker_id => 1, :color_id => 2})
+			expect(test_colorway.sneaker_id).to eq 1
+		end
 	end
 	
-
-
-
 
 
 end
